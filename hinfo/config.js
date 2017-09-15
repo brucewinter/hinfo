714741f4e84d80d9f9a491f78fed107aff28bd0b
Bruce Winter <brucewinter@gmail.com>
2017-09-15 16:47:36 -0500

    Update config.js

--- hinfo/config.js
+++ hinfo/config.js
@@ -1,4 +1,4 @@
 
-var pb_key     = 'o.daPpkzz8GM7d1FoLJGTLLcecbmmSruqD';
-var xively_key = 'C3Fsyg875-Wkk4_r9lWbvG8g6mKSAKxwbVF3Nkt6cDNMQT0g';
-var camera_url = 'http://192.168.0.112:8092/cgi-bin/CGIProxy.fcgi?cmd=snapPicture2&usr=admin&pwd=misterhouse';
+var pb_key     = 'your_key';
+var xively_key = 'your_key';
+var camera_url = 'http://camera_address/path_to_jpeg';
